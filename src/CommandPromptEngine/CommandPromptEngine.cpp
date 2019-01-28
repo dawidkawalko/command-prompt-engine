f757b16603ec9c671348dc2fc3bbeef92ec4d44e
Dawid Kawalko <dawid.kawalko.97@gmail.com>
2019-01-28 19:47:34 +0100

    Add main Engine files.
    No drawing implemented yet. Just the initial console setup and error
    checking.

---
+++ src/CommandPromptEngine/CommandPromptEngine.cpp
@@ -0,0 +1,88 @@
+#include "CommandPromptEngine.hpp"
+
+CommandPromptEngine::CommandPromptEngine()
+	: m_screenWidth(0), m_screenHeight(0), m_isRunning(false),
+	  m_consoleOutHandle(INVALID_HANDLE_VALUE), m_consoleRectangle { 0, 0 }
+{
+
+}
+
+CommandPromptEngine::~CommandPromptEngine()
+{
+
+}
+
+bool CommandPromptEngine::setSize(const int width, const int height, const int fontWidth, const int fontHeight)
+{
+	// Check if the provided size is correct
+	if (width <= 0 || height <= 0 || fontWidth <= 0 || fontHeight <= 0)
+	{
+		return false;
+	}
+
+	m_screenWidth = width;
+	m_screenHeight = height;
+
+	// Get the console output handle
+	m_consoleOutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (m_consoleOutHandle == INVALID_HANDLE_VALUE)
+	{
+		return false;
+	}
+
+	// Set the console size to 1x1 initially
+	m_consoleRectangle = { 0, 0, 1, 1 };
+	if (!SetConsoleWindowInfo(m_consoleOutHandle, true, &m_consoleRectangle))
+	{
+		return false;
+	}
+
+	// Set the console screen buffer size
+	COORD screenBufferSize = { short(m_screenWidth), short(m_screenHeight) };
+	if (!SetConsoleScreenBufferSize(m_consoleOutHandle, screenBufferSize))
+	{
+		return false;
+	}
+
+	// Set the active console screen buffer
+	if (!SetConsoleActiveScreenBuffer(m_consoleOutHandle))
+	{
+		return false;
+	}
+
+	// Set the font
+	CONSOLE_FONT_INFOEX fontInfo;
+	fontInfo.cbSize = sizeof(fontInfo);
+	fontInfo.nFont = 0;
+	fontInfo.dwFontSize.X = fontWidth;
+	fontInfo.dwFontSize.Y = fontHeight;
+	fontInfo.FontFamily = FF_DONTCARE;
+	fontInfo.FontWeight = FW_NORMAL;
+	wcscpy_s(fontInfo.FaceName, L"Consolas");
+
+	if (!SetCurrentConsoleFontEx(m_consoleOutHandle, false, &fontInfo))
+	{
+		return false;
+	}
+
+	// Set the actual console size
+	m_consoleRectangle = { 0, 0, short(m_screenWidth) - 1, short(m_screenHeight) - 1 };
+	if (!SetConsoleWindowInfo(m_consoleOutHandle, true, &m_consoleRectangle))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void CommandPromptEngine::start()
+{
+	// Call user startup method
+	m_isRunning = userStartup();
+
+	while (m_isRunning)
+	{
+		// Call user update method
+		m_isRunning = userUpdate();
+	}
+}
---
+++ src/CommandPromptEngine/CommandPromptEngine.hpp
@@ -0,0 +1,68 @@
+#ifndef __CommandPromptEngine__
+#define __CommandPromptEngine__
+
+#include <Windows.h>
+
+/**
+* Main class from which any user code should inherit.
+*
+* Quick guide:
+* > Create a class which inherits from CommandPromptEngine
+* > Overwrite the userStartup() and userUpdate() methods
+* > Create an instance of your new class
+* > Call the start(...) method with
+*/
+class CommandPromptEngine
+{
+public:
+	/**
+	* Default constructor.
+	*/
+	CommandPromptEngine();
+
+	/**
+	* Default destructor. Remember to write your own, if needed.
+	*/
+	virtual ~CommandPromptEngine();
+
+	/**
+	* Set the console parameters.
+	*
+	* @param width Number of cells in a row.
+	* @param height Number of cells in a column.
+	* @param fontWidth Width of a single cell.
+	* @param fontHeight Height of a single cell.
+	* @return bool True on success.
+	*/
+	bool setSize(const int width, const int height, const int fontWidth, const int fontHeight);
+
+	/**
+	* Start the engine.
+	*/
+	void start();
+
+protected:
+	/**
+	* Only called once. Use this method for initial setup.
+	*
+	* @return bool True on success.
+	*/
+	virtual bool userStartup() = 0;
+
+	/**
+	* Called on every game step.
+	*
+	* @return bool True on success.
+	*/
+	virtual bool userUpdate() = 0;
+
+private:
+	int m_screenWidth;
+	int m_screenHeight;
+	bool m_isRunning;
+
+	HANDLE m_consoleOutHandle;
+	SMALL_RECT m_consoleRectangle;
+};
+
+#endif // __CommandPromptEngine__
